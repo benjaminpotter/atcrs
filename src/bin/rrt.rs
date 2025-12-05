a9ed6d58c091a94d58a6e031de18cf12defbd374
Ben Potter <bp@benpot.com>
2025-12-05 18:17:22 -0500

    rrt works, but its slow, and the costs are wonky...

--- src/bin/rrt.rs
+++ src/bin/rrt.rs
@@ -1,28 +1,49 @@
-use std::{f64::consts::E, marker::PhantomData};
+use std::{
+    collections::{HashSet, VecDeque},
+    f64::consts::{E, PI},
+    fs::File,
+    io::{BufWriter, Write},
+    marker::PhantomData,
+    path::Path,
+};
 
 use dubins_paths::DubinsPath;
 use rand::{Rng, SeedableRng, rngs::SmallRng};
 
 fn main() {
-    let max_iters = 1;
-    let sampler = Sampler::new(0.01);
+    let max_iters = 10_000;
+    let sampler = Sampler::new(
+        (State([-75., -75., 0., 0.]), State([75., 75., 7., 2. * PI])),
+        0.01,
+    );
     let planner = RRTPlanner::new();
 
-    let start = State([-50., 0., 0., 0.]);
+    let start = State([-50., 0., 5., 0.]);
     let goal = State([0., 0., 0., 0.]);
     let mut plan = planner.create_plan(start, goal, 0);
 
-    for _ in 0..max_iters {
-        plan = planner.plan_from(plan, &sampler);
+    let polling_frequency = 100;
+    let outer = max_iters / polling_frequency;
+    for i in 0..outer {
+        for _ in 0..polling_frequency {
+            plan = planner.plan_from(plan, &sampler);
+        }
+        println!("[{}/{max_iters}]", (i + 1) * polling_frequency);
     }
+
+    println!("done");
+
+    // plan.print_to_file("rrt_plan.g");
+    plan.print_solution_to_file("rrt_plan.g");
 }
 
 trait Statelike {
     fn dim() -> usize;
 }
 
-#[derive(Clone, Debug)]
+#[derive(Clone, Debug, PartialEq)]
 struct State([f64; 4]);
+
 impl State {
     fn dist(&self, state: &State) -> f64 {
         ((state.0[0] - self.0[0]).powf(2.)
@@ -39,11 +60,10 @@ impl Statelike for State {
     }
 }
 
-struct Vertex<'a> {
-    parent: Option<&'a Vertex<'a>>,
+struct Vertex {
+    parent: Option<usize>,
     state: State,
     cost_to_come: f64,
-    children: Vec<&'a Vertex<'a>>,
 }
 
 enum Sample {
@@ -52,52 +72,103 @@ enum Sample {
 }
 
 struct Sampler {
+    min: State,
+    max: State,
     goal_sampling_chance: f64,
 }
 
 impl Sampler {
+    fn new(corners: (State, State), goal_sampling_chance: f64) -> Self {
+        Self {
+            min: State([
+                corners.0.0[0].min(corners.1.0[0]),
+                corners.0.0[1].min(corners.1.0[1]),
+                corners.0.0[2].min(corners.1.0[2]),
+                corners.0.0[3].min(corners.1.0[3]),
+            ]),
+            max: State([
+                corners.0.0[0].max(corners.1.0[0]),
+                corners.0.0[1].max(corners.1.0[1]),
+                corners.0.0[2].max(corners.1.0[2]),
+                corners.0.0[3].max(corners.1.0[3]),
+            ]),
+            goal_sampling_chance,
+        }
+    }
+
     fn sample<R: Rng>(&self, rng: &mut R) -> Sample {
         if !rng.random_bool(self.goal_sampling_chance) {
-            let mut state = [0.0; 4];
-            rng.fill(&mut state);
+            let mut state = [
+                rng.random_range(self.min.0[0]..self.max.0[0]),
+                rng.random_range(self.min.0[1]..self.max.0[1]),
+                rng.random_range(self.min.0[2]..self.max.0[2]),
+                rng.random_range(self.min.0[3]..self.max.0[3]),
+            ];
             return Sample::State(State(state));
         }
 
         Sample::Goal
     }
-
-    fn new(goal_sampling_chance: f64) -> Self {
-        Self {
-            goal_sampling_chance,
-        }
-    }
 }
 
-struct World<'a> {
-    vertices: Vec<Vertex<'a>>,
+struct World {
+    vertices: Vec<Vertex>,
 }
 
-impl<'a> World<'a> {
+impl World {
     pub fn new(start: State) -> Self {
         let vertices = vec![Vertex {
             parent: None,
             state: start,
             cost_to_come: 0.,
-            children: Vec::new(),
         }];
 
         World { vertices }
+    }
+
+    fn root(&self) -> usize {
+        0
+    }
+
+    fn all(&self) -> Vec<usize> {
+        (0..self.vertices.len()).collect()
     }
 
     fn state(&self, ix: usize) -> Option<&State> {
         self.vertices.get(ix).map(|v| &v.state)
     }
 
+    fn cost_to_come(&self, ix: usize) -> Option<f64> {
+        self.vertices.get(ix).map(|v| v.cost_to_come)
+    }
+
+    fn cost_to_come_mut(&mut self, ix: usize) -> Option<&mut f64> {
+        self.vertices.get_mut(ix).map(|v| &mut v.cost_to_come)
+    }
+
+    fn insert(&mut self, state: State) -> usize {
+        self.vertices.push(Vertex {
+            parent: None,
+            state,
+            cost_to_come: f64::INFINITY,
+        });
+        self.vertices.len() - 1
+    }
+
+    fn parent(&self, ix: usize) -> Option<Option<usize>> {
+        self.vertices.get(ix).map(|v| v.parent)
+    }
+
+    fn parent_mut(&mut self, ix: usize) -> Option<&mut Option<usize>> {
+        self.vertices.get_mut(ix).map(|v| &mut v.parent)
+    }
+
     pub fn state_count(&self) -> usize {
         self.vertices.len()
     }
 
-    pub fn k_nearest(&'a self, state: &State, k: usize) -> Vec<usize> {
+    // TODO: THIS IS SUPER SLOW...
+    pub fn k_nearest(&self, state: &State, k: usize) -> Vec<usize> {
         let mut distances: Vec<_> = self
             .vertices
             .iter()
@@ -124,23 +195,28 @@ impl RRTPlanner {
         start: State,
         goal: State,
         seed: u64,
-    ) -> RRTPlan<'a, SmallRng, State> {
+    ) -> RRTPlan<SmallRng, State> {
         RRTPlan {
             world: World::new(start),
             rng: SmallRng::seed_from_u64(seed),
             goal,
+            goal_ix: None,
             k_factor: E * (1. + 1. / State::dim() as f64),
             phan: PhantomData,
         }
     }
 
-    pub fn plan_from<'a>(
+    pub fn plan_from(
         &self,
-        mut plan: RRTPlan<'a, SmallRng, State>,
+        mut plan: RRTPlan<SmallRng, State>,
         sampler: &Sampler,
-    ) -> RRTPlan<'a, SmallRng, State> {
+    ) -> RRTPlan<SmallRng, State> {
+        let mut is_goal = false;
         let sample = match sampler.sample(plan.rng()) {
-            Sample::Goal => plan.goal.clone(),
+            Sample::Goal => {
+                is_goal = true;
+                plan.goal.clone()
+            }
             Sample::State(state) => state,
         };
 
@@ -166,46 +242,89 @@ impl RRTPlanner {
             // just try a different sample...
             return plan;
         };
-        let t_min = shortest_path.length() / xy_velocity;
+        let xy_dist = shortest_path.length();
+        let t_min = xy_dist / xy_velocity;
         let max_alt_change = max_alt_rate * t_min;
-        let sample_alt_change = nearest_state.0[2] - sample.0[2];
-        let alt_change = if max_alt_change.abs() < sample_alt_change.abs() {
-            max_alt_change
-        } else {
-            sample_alt_change
-        };
+        // Clamp the alt_change to prevent us from violating our constraints.
+        let alt_change = (nearest_state.0[2] - sample.0[2]).clamp(-max_alt_change, max_alt_change);
         let mut state = sample;
-        state.0[2] = alt_change;
+        state.0[2] += alt_change;
 
-        // No edge check required because we don't have any hard obstacles.
+        if is_goal && plan.goal_ix.is_some() {
+            // Already found the goal.
+            return plan;
+        }
 
-        let k_nearest = plan.world.k_nearest(&nearest_state, plan.k_bound());
+        let k_nearest = plan.world.k_nearest(&state, plan.k_bound());
 
         // Connect this state to the state which provides the lowest cost-to-come.
-        let parent = k_nearest.iter().map(|ix| {
-            (
-                ix,
-                plan.world.cost_to_come(ix) + state.dist(plan.world.state(ix)),
-            )
-        }); // TODO: take the min of this list by the cost_to_come, then use that ix as the parent
+        let (parent_ix, cost_to_come) = k_nearest
+            .iter()
+            .map(|ix| {
+                (
+                    ix,
+                    plan.world
+                        .cost_to_come(*ix)
+                        .expect("k_nearest gives valid ids")
+                        + state.dist(plan.world.state(*ix).expect("k_nearest gives valid ids")),
+                )
+            })
+            .min_by(|(_, a), (_, b)| a.partial_cmp(&b).expect("costs are well-ordered"))
+            .expect("length of k_nearest > 0");
 
-        // Add this state to the world.
+        let ix = plan.world.insert(state.clone());
+        *plan.world.parent_mut(ix).unwrap() = Some(*parent_ix);
+        *plan.world.cost_to_come_mut(ix).unwrap() = cost_to_come;
+
+        if is_goal {
+            plan.goal_ix = Some(ix);
+        }
 
         // Locally optimize...
+        for child_ix in k_nearest.iter().filter(|cix| *cix != parent_ix) {
+            let child_state = plan.world.state(*child_ix).unwrap();
+            let child_cost_to_come = cost_to_come + state.dist(child_state);
+            if plan.world.cost_to_come(*child_ix).unwrap() <= child_cost_to_come {
+                // Already optimal.
+                continue;
+            }
+
+            // Check whether this edge violates our constraints.
+            let q0 = [state.0[0], state.0[1], state.0[3]].into();
+            let q1 = [child_state.0[0], child_state.0[1], child_state.0[3]].into();
+            let Ok(shortest_path) = DubinsPath::shortest_from(q0, q1, rho) else {
+                // Failed to find a path; we assume this means this edge violates the motion constraints.
+                continue;
+            };
+            let xy_dist = shortest_path.length();
+            let t_min = xy_dist / xy_velocity;
+            let max_alt_change = max_alt_rate * t_min;
+            let alt_change = state.0[2] - child_state.0[2];
+            if max_alt_change > alt_change.abs() {
+                // This edge violates the motion constraints.
+                continue;
+            }
+
+            // We found a better path, which does not violate any constraints, and lets use it.
+            // TODO: This API is bad because it would let us set the parent to None.
+            *plan.world.parent_mut(*child_ix).unwrap() = Some(ix);
+            *plan.world.cost_to_come_mut(*child_ix).unwrap() = child_cost_to_come;
+        }
 
         plan
     }
 }
 
-struct RRTPlan<'a, R, S> {
-    world: World<'a>,
+struct RRTPlan<R, S> {
+    world: World,
     rng: R,
     goal: State,
+    goal_ix: Option<usize>,
     k_factor: f64,
     phan: PhantomData<S>,
 }
 
-impl<'a, R, S> RRTPlan<'a, R, S> {
+impl<R, S> RRTPlan<R, S> {
     fn rng(&mut self) -> &mut R
     where
         R: Rng,
@@ -217,5 +336,58 @@ impl<'a, R, S> RRTPlan<'a, R, S> {
         let card = self.world.state_count() as f64;
         let bound = (self.k_factor * card.log2()).ceil() as usize;
         bound.clamp(1, usize::MAX)
+    }
+
+    fn print_to_file<P: AsRef<Path>>(&self, path: P) {
+        println!("state count: {}", self.world.state_count());
+
+        let file = File::create(&path).unwrap();
+        let mut writer = BufWriter::new(file);
+        // let mut writer = std::io::stdout();
+
+        for ix in self.world.all() {
+            let state = self.world.state(ix).unwrap();
+            let cost_to_come = self.world.cost_to_come(ix).unwrap();
+
+            let _ = writeln!(
+                writer,
+                "v {} {:.2} {:.2} {:.2} {:.2} {:.2} {:.2}",
+                ix, state.0[0], state.0[1], state.0[2], state.0[3], cost_to_come, 0.,
+            );
+
+            if let Some(parent_ix) = self.world.parent(ix).unwrap() {
+                let _ = writeln!(writer, "e {} {}", parent_ix, ix);
+            }
+        }
+    }
+
+    fn print_solution_to_file<P: AsRef<Path>>(&self, path: P) {
+        let Some(goal_ix) = self.goal_ix else {
+            eprintln!("no solution found");
+            return;
+        };
+
+        let file = File::create(&path).unwrap();
+        let mut writer = BufWriter::new(file);
+        // let mut writer = std::io::stdout();
+
+        let mut ix = goal_ix;
+        loop {
+            let state = self.world.state(ix).unwrap();
+            let cost_to_come = self.world.cost_to_come(ix).unwrap();
+
+            let _ = writeln!(
+                writer,
+                "v {} {:.2} {:.2} {:.2} {:.2} {:.2} {:.2}",
+                ix, state.0[0], state.0[1], state.0[2], state.0[3], cost_to_come, 0.,
+            );
+
+            let Some(parent_ix) = self.world.parent(ix).unwrap() else {
+                break;
+            };
+
+            let _ = writeln!(writer, "e {} {}", parent_ix, ix);
+            ix = parent_ix;
+        }
     }
 }
